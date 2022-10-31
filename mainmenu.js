0b10ebdf006633a0f4dd66661194bb05f2d45357
rsenecal <roodysenecal@gmail.com>
2022-10-31 19:21:45 -0400

    Cleaning uo menu

--- mainmenu.js
+++ mainmenu.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-
-
+// const logo = require('asciiart-logo');
+// const config = require('./package.json');
 
 const mysql = require('mysql2');
 
@@ -14,14 +14,8 @@ const connection = mysql.createConnection({
 
 
 
-console.log(
-    `=============================================================================================================
-    '                  E M P L O Y E E     T R A C K E R - M O D U L E   1 2                                      
-    ==============================================================================================================
-    
-    
-    `
-  );
+// console.log(logo(config).render());
+
 
 const mainMenu = () => {
     return inquirer.prompt ([
@@ -29,7 +23,32 @@ const mainMenu = () => {
             type: 'list',
             name: 'mainmenu',
             message: 'What would you like to do? :',
-            choices: ['View All Employee', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+            choices: [
+                new inquirer.Separator('= View ='),
+                {
+                    name: 'View All Employee',
+                },
+                {
+                    name:  'View All Roles',
+                },
+                {
+                    name: 'View All Departments',
+                },
+                new inquirer.Separator('= Add =='),
+                {
+                    name: 'Add Employee',
+                },
+                {
+                    name: 'Add Role',
+                },
+                {
+                    name: 'Add Department',
+                },
+                new inquirer.Separator('= Update =='),
+                {
+                    name: 'Update Employee Role',
+                },
+              ],
         }
     ])
 
@@ -38,21 +57,24 @@ const mainMenu = () => {
         case "View All Employee":
             allEmployee();
             break;
-        case "Add Employee":
-            addEmployee();
-            break;
-        case "Update Employee Role":
-            updateEployee();
-            break;
         case "View All Roles":
             allRoles();
-            break;
-        case "Add Role":
-            addRole();
             break;
         case "View All Departments":
             allDepartments();
             break;
+
+        case "Update Employee Role":
+            updateEployee();
+            break;
+
+        case "Add Employee":
+            promptEmployee();
+            break;
+        case "Add Role":
+                addRole();
+        break;
+
         case "Add Department":
             addDepartments();
             break;
@@ -87,13 +109,13 @@ const promptEmployee = ()  => {
     },
     {
         type: 'input',
-        name: 'first_name',
-        message: "Please the employee's first name: ",
-        validate: first_name => {
-            if (first_name) {
+        name: 'last_name',
+        message: "Please the employee's last name: ",
+        validate: last_name => {
+            if (last_name) {
                 return true
             } else {
-                console.log('Please try again, Enter Empoyee first name: ')
+                console.log('Please try again, Enter Empoyee last name: ')
             }
         }
     },
